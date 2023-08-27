e8c06dd274640233c3dde9457f32f955998fe1ff
VTC-105-11 <VTC-105-11@DESKTOP-R6GJED0>
2023-08-27 20:39:58 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MyShop;
+using MyShop.Entities;
+
+namespace MyShopConsole;
+
+
+    internal class Program
+    {
+    static void Main(string[] args)
+    {
+
+        ShopDataContext context = new();
+        Product product = new()
+        {
+           
+            Discount = 1,
+            CategoryId = 1,
+            Name = "Test",
+            Description = "Test smth",
+            VendorId = 1,
+  
+        };
+
+
+        context.Products.Add(product);
+
+        var result = context.SaveChanges();
+
+
+    }  
+}
+    
